2084af1cdc4835735ad7ca1ab3ffd31068d27a4a
Tom Buscher <tom@storycloud.co>
2020-10-05 16:28:46 -0700

    Fix release migration task

--- lib/utils/rel/task.ex
+++ lib/utils/rel/task.ex
@@ -1,11 +1,10 @@
 defmodule Utils.Rel.Task do
-  alias Depos.{Repo}
-
-  def migrate(app) do
+  def migrate(app, repo) do
     with app = String.to_atom(app),
+         repo = String.to_atom(repo),
          {:ok, _} = Application.ensure_all_started(app),
          path = Application.app_dir(app, "priv/repo/migrations") do
-      Ecto.Migrator.run(Repo, path, :up, all: true)
+      Ecto.Migrator.run(repo, path, :up, all: true)
     end
   end
 end
